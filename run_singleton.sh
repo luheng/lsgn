c40e57188724284baa80fd036cbf3de25bc788f2
Luheng He <luheng@uw.edu>
2018-05-03 22:19:23 -0700

    Remove.

--- run_singleton.sh
+++
@@ -1,5 +0,0 @@
-until python singleton.py $1; do
-    echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
-    sleep 1
-done
-
--- scripts/rename_variables.sh
+++
@@ -1,4 +0,0 @@
-python rename_variables.py \
---checkpoint_dir=$1  \
---replace_from="lm_aggregation/lm_" \
---replace_to="module/aggregation/" 
