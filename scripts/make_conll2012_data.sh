317432377619d2ab479c8249abe6fa860e97859c
Luheng He <luheng@uw.edu>
2018-05-16 19:25:35 -0700

    CoNLL2012 filtering.

--- scripts/make_conll2012_data.sh
+++ scripts/make_conll2012_data.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-#ONTONOTES_PATH=/home/luheng/Data/conll-formatted-ontonotes-5.0
 ONTONOTES_PATH=$1
-#bash conll-2012/v3/scripts/skeleton2conll.sh -D $ontonotes_path/data/files/data conll-2012
 
 SRL_PATH="./data/srl"
 
 if [ ! -d $SRLPATH ]; then
   mkdir -p $SRLPATH
 fi
+
+
+# Preprocess CoNLL formatted files.
 
 rm -f ${SRL_PATH}/train.english.v5_gold_conll
 cat ${ONTONOTES_PATH}/data/train/data/english/annotations/*/*/*/*.gold_conll \
@@ -22,13 +23,32 @@ rm -f ${SRL_PATH}/conll12test.english.v5_gold_conll
 cat ${ONTONOTES_PATH}/data/conll-2012-test/data/english/annotations/*/*/*/*.gold_conll \
   >> ${SRL_PATH}/conll12test.english.v5_gold_conll
 
-
 python scripts/ontonotes5_to_json.py ${SRL_PATH}/train.english.v5_gold_conll \
   ${SRL_PATH}/train.english.v5.jsonlines
 python scripts/ontonotes5_to_json.py ${SRL_PATH}/dev.english.v5_gold_conll \
   ${SRL_PATH}/dev.english.v5.jsonlines
 python scripts/ontonotes5_to_json.py ${SRL_PATH}/conll12test.english.v5_gold_conll \
   ${SRL_PATH}/conll12test.english.v5.jsonlines
+
+
+# Filter data for e2e experiments.
+
+wget http://conll.cemantix.org/2012/download/ids/english/coref/train.id -O ${SRL_PATH}/conll12.train.id
+wget http://conll.cemantix.org/2012/download/ids/english/coref/development.id -O ${SRL_PATH}/conll12.dev.id
+wget http://conll.cemantix.org/2012/download/ids/english/coref/test.id -O ${SRL_PATH}/conll12.test.id
+
+python scripts/filter_conll2012_data.py ${SRL_PATH}/train.english.v5.jsonlines \
+  ${SRL_PATH}/conll12.train.id \
+  ${SRL_PATH}/train.english.mtl.jsonlines
+
+python scripts/filter_conll2012_data.py ${SRL_PATH}/dev.english.v5.jsonlines \
+  ${SRL_PATH}/conll12.dev.id \
+  ${SRL_PATH}/dev.english.mtl.jsonlines
+
+python scripts/filter_conll2012_data.py ${SRL_PATH}/conll12test.english.v5.jsonlines \
+  ${SRL_PATH}/conll12.test.id \
+  ${SRL_PATH}/test.english.mtl.jsonlines
+
 
 #python scripts/get_char_vocab.py
 #python filter_embeddings.py glove.840B.300d.txt train.english.jsonlines dev.english.jsonlines test.english.jsonlines
